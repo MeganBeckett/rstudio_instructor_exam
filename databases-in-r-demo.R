583bc0ff34ef3bd59d5ff6adc469a0548dd1a836
Megan Beckett <megan.beckett13@gmail.com>
2020-01-20 13:26:49 +0200

    Rename file

--- databases-in-r-demo.R
+++ databases-in-r-demo.R
@@ -1,30 +1,35 @@
 # =========================================================================================== #
 #                                                                                             #
-# Connecting to a database                                                      Megan Beckett #
+# Connecting to a database - demo                                               Megan Beckett #
 # =========================================================================================== #
 
-
 # DATA ----------------------------------------------------------------------------------------
-# Run the following if doing this yourself and you want the data.
-# On RStudio Cloud, the data is already present
-dir.create("data_raw", showWarnings = FALSE)
-download.file(url = "https://ndownloader.figshare.com/files/2292171",
-              destfile = "data_raw/mammals.sqlite", mode = "wb")
 
+# Run the following if doing this yourself and you want the data.
+# On RStudio Cloud, the data is already present.
+#
+dir.create("data_raw", showWarnings = FALSE)
+download.file(
+  url = "https://ndownloader.figshare.com/files/2292171",
+  destfile = "data_raw/mammals.sqlite",
+  mode = "wb"
+)
 
 # LIBRARIES -----------------------------------------------------------------------------------
+
 library(dplyr)
 library(dbplyr)
 
-
 # CONNECTION ----------------------------------------------------------------------------------
-mammals <- DBI::dbConnect(RSQLite::SQLite(), "data_raw/mammals.sqlite")
 
+mammals <- DBI::dbConnect(RSQLite::SQLite(), "data/mammals.sqlite")
 
 # INVESTIGATE ---------------------------------------------------------------------------------
+
 src_dbi(mammals)
 
-# Create a table from a data source
+# Create a table from a data source.
+#
 surveys <- tbl(mammals, "surveys")
 
 head(surveys)
@@ -37,15 +42,16 @@ surveys_data <- surveys %>%
 nrow(surveys_data)
 
 # QUERY ---------------------------------------------------------------------------------------
+
 # How many different species per taxa are in the dataset?
+#
 species <- tbl(mammals, "species")
 
 head(species)
 
 taxa_summary <- species %>%
-  group_by(taxa) %>%
-  summarise(N = n()) %>%
+  count(taxa) %>%
   collect()
 
-
-
+show_query(species %>%
+             count(taxa))
