75a4725f07ba3e064d3b04ede648ae849e572621
Megan Beckett <megan.beckett13@gmail.com>
2020-01-20 16:16:00 +0200

    Add framework for demo

---
+++ databases-in-r-demo-framework.R
@@ -0,0 +1,35 @@
+# =========================================================================================== #
+#                                                                                             #
+# Connecting to a database - demo                                               Megan Beckett #
+# =========================================================================================== #
+
+# DATA ----------------------------------------------------------------------------------------
+
+# Run the following if doing this yourself and you want the data.
+# On RStudio Cloud, the data is already present.
+#
+dir.create("data_raw", showWarnings = FALSE)
+download.file(
+  url = "https://ndownloader.figshare.com/files/2292171",
+  destfile = "data_raw/mammals.sqlite",
+  mode = "wb"
+)
+
+# LIBRARIES -----------------------------------------------------------------------------------
+
+
+# CONNECTION ----------------------------------------------------------------------------------
+
+
+# INVESTIGATE ---------------------------------------------------------------------------------
+
+
+# Create a table from a data source.
+#
+
+
+# QUERY ---------------------------------------------------------------------------------------
+
+# How many different species per taxa are in the dataset?
+#
+
